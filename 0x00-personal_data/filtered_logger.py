868488af3cce3f9e2c26f14a3e054cf7e28a3412
segunUb18 <eeomo@yahoo.com>
2023-08-03 16:42:48 +0100

    :zap: Improve filtered logger
    improve filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,8 @@ import os
 
 
 patterns = {
-        'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-        'replace': lambda x: r'\g<field>={}'.format(x),
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
 }
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
@@ -16,7 +16,27 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
-        ) -> str:
+) -> str:
     """Filters a log line."""
     extract, replace = (patterns['extract'], patterns['replace'])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMATS_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+            """ Redacting Formatter class """
+            msg = super(RedactingFormatter, self).format(record)
+            txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+            return txt
+if __name__ == "__main__":
+    main()
