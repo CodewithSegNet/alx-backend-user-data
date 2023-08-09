cdca9421d523e44f0024fc5911007ad37e9c13d6
segunUb18 <eeomo@yahoo.com>
2023-08-09 17:23:46 +0100

    :zap: Create an instance method user id for session id
    a method that returns the value(the User ID) for the key session id in the dictionary

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,43 @@
 #!/usr/bin/env python3
 """ a class that inherits from Auth"""
 from api.v1.auth.auth import Auth
+import uuid
+
 
 class SessionAuth(Auth):
-    pass
+    """ class attr to store user_id by session_id
+    """
+    user_id_by_session_id = {}
+    
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a user_id.
+
+        Args:
+            user_id (str): User ID
+        Returns:
+            str: Session ID.
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+    
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+            """ Retrieve User ID based on a Session ID
+
+            Args:
+                session_id (str): Session ID.
+            
+            Returns:
+                str: User ID associated with the Session ID
+            """
+            if session_id is None or not isinstance(session_id, str):
+                return None
+
+            return self.user_id_by_session_id.get(session_id, None)
+
 
 if __name__ == '__main__':
     session_auth = SessionAuth()
