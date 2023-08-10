0fb00e974bb4272f70e447df627446a1463a22e6
segunUb18 <eeomo@yahoo.com>
2023-08-10 13:27:31 +0100

    :art: Edit session to follow pycodestyle
    edit for pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,7 @@ class SessionAuth(Auth):
     """ class attr to store user_id by session_id
     """
     user_id_by_session_id = {}
-    
+
     def create_session(self, user_id: str = None) -> str:
         """ Create a Session ID for a user_id.
 
@@ -19,24 +19,24 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-    
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
-            """ Retrieve User ID based on a Session ID
+        """ Retrieve User ID based on a Session ID
 
-            Args:
-                session_id (str): Session ID.
-            
-            Returns:
-                str: User ID associated with the Session ID
-            """
-            if session_id is None or not isinstance(session_id, str):
-                return None
+        Args:
+            session_id (str): Session ID.
 
-            return self.user_id_by_session_id.get(session_id, None)
+        Returns:
+            str: User ID associated with the Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id, None)
 
 
 if __name__ == '__main__':
